bd6085ab1ed0f6ad66d2bd39486cf9d0f9f28eb5
Asfandyar <a_asfandyar1@hotmail.com>
2023-09-22 03:35:17 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mac User on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
